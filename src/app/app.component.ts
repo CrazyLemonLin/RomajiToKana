b32b04f592b0cc5c110aa6bea95dfe02c0fdc78b
CrazyLemonLin <lemonlinchju@hotmail.com>
2017-06-05 01:13:49 +0800

    抽換套件wanakana -> kuroshiro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+// import { Window } from './definitions/wanakana.d';
+import { Component, OnInit } from '@angular/core';
+//import { wanakana } from "typings";
+declare var kuroshiro: any;
 @Component({
   selector: 'cyl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cyl';
+export class AppComponent implements OnInit {
+
+  input: string;
+  output: string;
+
+  /**
+   *constructor
+   */
+  constructor() { }
+
+  ngOnInit(): void {
+    this.input = '時は行く　僕達は　すれ違い　また出会う';
+    kuroshiro.init();
+    this.output = kuroshiro.convert(this.input, { mode: 'okurigana', to: 'hiragana' });
+  }
 }
