ec48551879530703b8d04d00ca45c58bb6f77031
CrazyLemon <lemonlinchju@hotmail.com>
2017-06-17 12:50:43 +0800

    create analysis component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalysisComponent
   ],
   imports: [
     BrowserModule,
