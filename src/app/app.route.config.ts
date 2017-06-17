97645ae05b465fc1462abf5fbcf33efb3af31088
CrazyLemon <lemonlinchju@hotmail.com>
2017-06-17 14:15:18 +0800

    完成router設定及加上navbar

--- src/app/app.route.config.ts
+++ src/app/app.route.config.ts
@@ -3,7 +3,7 @@ import { ConverterComponent } from './converter/converter.component';
 import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   {
-    path: 'conveter', component: ConverterComponent
+    path: 'converter', component: ConverterComponent
   },
   {
     path: 'analysis', component: AnalysisComponent
